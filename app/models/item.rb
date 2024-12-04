ff975392638505d12c341e5c931925191cace2d0
Michael Tuhaimer <michaeltuhaimer@Michaels-MacBook-Air.local>
2024-12-04 08:22:48 -0600

    Add Orders

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  has_many :orders
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_03_164230) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_141634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_164230) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.decimal "subtotal", precision: 9, scale: 2
+    t.decimal "tax", precision: 9, scale: 2
+    t.decimal "total", precision: 9, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -32,4 +43,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_164230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
